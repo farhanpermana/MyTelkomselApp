e6cbdc4e0ab1ca72fc40681a6c02753449a13bd0
farhanpermana <46885640+farhanpermana@users.noreply.github.com>
2023-03-06 17:12:03 +0700

    init popularcollectioncell

---
+++ MyTelkomselApp/Pages/Paket Internet Page/View/PopularCollectionCell/PopularCollectionCell/PopularCollectionCell.swift
@@ -0,0 +1,19 @@
+//
+//  PopularCollectionCell.swift
+//  MyTelkomselApp
+//
+//  Created by Phinconers on 06/03/23.
+//
+
+import UIKit
+
+class PopularCollectionCell: UICollectionViewCell {
+    
+    static let identifier = "PopularCollectionCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ MyTelkomselApp/Pages/Paket Internet Page/View/PopularCollectionCell/PopularTableCell.swift
@@ -0,0 +1,79 @@
+//
+//  PopularTableCell.swift
+//  MyTelkomselApp
+//
+//  Created by Phinconers on 06/03/23.
+//
+
+import UIKit
+
+class PopularTableCell: UITableViewCell {
+    
+    static let identifier = "PopularTableCell"
+    
+    var popularDatas: [PaketModel] = []
+    
+    private var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UINib(nibName: "PopularCollectionCell", bundle: nil), forCellWithReuseIdentifier: PopularCollectionCell.identifier)
+        collectionView.layer.masksToBounds = false
+//        collectionView.backgroundColor = .green
+
+        return collectionView
+    }()
+    
+    func setupCollectionView() {
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
+    }
+    
+    func setupTable() {
+        contentView.addSubview(collectionView)
+        setupCollectionView()
+        collectionView.backgroundColor = UIColor.clear
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+
+extension PopularTableCell: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return popularDatas.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LanggananCollectionCell.identifier, for: indexPath) as? LanggananCollectionCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 5, left: 16, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width / 1.5, height: 130)
+    }
+    
+    
+}
