3e0eeb8b9f23ee6335bc2fbfc9f61309146ac6c0
farhanpermana <46885640+farhanpermana@users.noreply.github.com>
2023-03-06 17:01:10 +0700

    Create CarouselModels.swift

---
+++ MyTelkomselApp/Models/CarouselModels.swift
@@ -0,0 +1,14 @@
+//
+//  CarouselModels.swift
+//  MyTelkomselApp
+//
+//  Created by Phinconers on 06/03/23.
+//
+
+import Foundation
+
+struct CarouselModel {
+    let imageLabel: String
+    let titleLabel: String
+    let subTitleLabel: String
+}
