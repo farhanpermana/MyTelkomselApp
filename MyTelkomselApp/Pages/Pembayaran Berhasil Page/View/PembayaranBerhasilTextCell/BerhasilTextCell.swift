25e3f000460825d9b884c92aa9ee285b4538570e
farhanpermana <46885640+farhanpermana@users.noreply.github.com>
2023-03-09 14:56:47 +0700

    Update BerhasilTextCell.swift

--- MyTelkomselApp/Pages/Pembayaran Berhasil Page/View/PembayaranBerhasilTextCell/BerhasilTextCell.swift
+++ MyTelkomselApp/Pages/Pembayaran Berhasil Page/View/PembayaranBerhasilTextCell/BerhasilTextCell.swift
@@ -31,8 +31,11 @@ class BerhasilTextCell: UITableViewCell {
         checkMarkImg.tintColor = UIColor.white
         
         firstView.layer.cornerRadius = firstView.frame.width / 2
+        firstView.layer.backgroundColor = UIColor(red: 0.925, green: 0.125, blue: 0.161, alpha: 1).cgColor
+        secondView.layer.backgroundColor = UIColor(red: 0.976, green: 0.831, blue: 0.859, alpha: 1).cgColor
         
         secondView.layer.cornerRadius = secondView.frame.width / 2
+        
         
         titleBerhasilLabel.font = UIFont.systemFont(ofSize: 24, weight: .bold)
         
